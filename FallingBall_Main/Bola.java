1d410d62c45b4de6b0c527472ef3722a0257f570
Joao Pereira <joaopereira_19@hotmail.com>
2016-07-11 19:50:38 +0100

    Fixed a mistake in the previous commit - I submitted the classes instead of the .java

---
+++ FallingBall_Main/Bola.java
@@ -0,0 +1,76 @@
+package FallingBall_Main;
+
+public class Bola {
+
+	private int centerX = 240;
+	private int centerY = 200;
+
+	private int speedX = 0;
+	private int speedY = 5;
+
+	public void update() {
+		centerX = centerX + speedX;
+
+		if (centerX < -5) {
+			centerX = 485;
+		} else if (centerX > 485) {
+			centerX = -5;
+		}
+
+		centerY = centerY + speedY;
+
+		if (centerY >= 735) {
+			speedY = 0;
+			centerY = 735;
+		}
+		
+		
+
+	}
+
+	public void ir_direita() {
+		speedX = 5;
+	}
+
+	public void ir_esquerda() {
+		speedX = -5;
+	}
+
+	public void stop() {
+		speedX = 0;
+	}
+
+	// Getters e Setters
+	public int getCenterX() {
+		return centerX;
+	}
+
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+
+	public int getCenterY() {
+		return centerY;
+	}
+
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+
+	public int getSpeedX() {
+		return speedX;
+	}
+
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+
+	public int getSpeedY() {
+		return speedY;
+	}
+
+	public void setSpeedY(int speedY) {
+		this.speedY = speedY;
+	}
+	// getters e setters
+}
---
+++ FallingBall_Main/Plataforma.java
@@ -0,0 +1,114 @@
+package FallingBall_Main;
+
+
+import java.util.Random;
+
+public class Plataforma {
+	private int centerY, centerX;
+	private int speedY = -5;
+	boolean vez_1 = true;
+	Random r = new Random();
+	Random w = new Random();
+	
+	
+
+	public void update(int Y) {
+		if (vez_1 == true) {
+			escolher();
+			centerY = Y;
+			vez_1 = false;
+
+		}
+		centerY = centerY + speedY;
+		if (centerY < 0) {
+			escolher();
+			centerY = 800;
+		}
+		
+
+	}
+
+	
+
+	private void escolher(){
+		int rand = r.nextInt(7);
+		switch (rand) {
+		case 0:
+			centerX = 50;
+			break;
+		case 1:
+			centerX = 150;
+			break;
+		case 2:
+			centerX = 200;
+			break;
+		case 3:
+			centerX = 250;
+			break;
+		case 4:
+			centerX = 300;
+			break;
+		case 5:
+			centerX = 350;
+			break;
+		case 6:
+			int rand2 = w.nextInt(3);
+			switch(rand2){
+			case 0:
+				centerX=-100;
+				break;
+			case 1:
+				centerX=430;
+				break;
+			case 2:
+				centerX = 75;
+				break;
+			}
+			break;
+			
+
+		}
+	}
+	
+	public int getCenterY() {
+		return centerY;
+	}
+
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+
+	public int getCenterX() {
+		return centerX;
+	}
+
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+
+	public int getSpeedY() {
+		return speedY;
+	}
+
+	public void setSpeedY(int speedY) {
+		this.speedY = speedY;
+	}
+
+
+
+	public boolean isVez_1() {
+		return vez_1;
+	}
+
+
+
+	public void setVez_1(boolean vez_1) {
+		this.vez_1 = vez_1;
+	}
+
+
+
+
+
+	
+}
